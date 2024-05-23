cad102ec3b05c0207524437d2d5a4f18ac8e6821
elmekkaoui <elmekkaoui@meet.local.org>
2024-05-23 10:31:58 +0100

    initial commit

---
+++ src/app/api/route.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from "next/server";
+
+// Create a POST API Route
+export async function POST(req: NextRequest) {
+  // Extract `theImage` sent from the frontend
+  const formData = await req.formData();
+  const theImage = formData.get("theImage");
+  const headers = { Authorization: `Bearer ${process.env.HF_APIKEY}` };
+  console.log('---------- headers: ', headers);
+  debugger;
+  // Make a call to Hugging Face using the API Key we got from there
+  const response = await fetch(
+    "https://api-inference.huggingface.co/models/facebook/detr-resnet-50-panoptic",
+    {
+      headers: headers,
+      method: "POST",
+      body: theImage,
+    }
+  );
+
+  // Get the response body
+  const result = await response.json();
+
+  // Forward the response back to the frontend
+  return NextResponse.json({ body: result });
+}
